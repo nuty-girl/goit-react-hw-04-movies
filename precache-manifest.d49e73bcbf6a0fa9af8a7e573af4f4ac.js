f66fbfb01a468e86bb441e9aaad34757a0deeedd
unknown <nuty-girl@mail.ru>
2019-11-30 15:57:02 +0200

    Updates

---
+++ precache-manifest.d49e73bcbf6a0fa9af8a7e573af4f4ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f6ff6ac425e9d13fd59f9fe2f698cfb",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "1736f44b26058afce42f",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3bf4ba786094f00d09c2",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.0100c391.chunk.js"
+  },
+  {
+    "revision": "1736f44b26058afce42f",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.47845f35.chunk.js"
+  },
+  {
+    "revision": "dc6c13955cd237d05295",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.41ee2551.js"
+  }
+]);
